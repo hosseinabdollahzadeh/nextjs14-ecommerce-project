23761abe52825f81cbe14cdb233a6d7853df0279
Hossein <abdollahzadeh.hossein@gmail.com>
2024-12-25 06:27:35 +0330

    Add progressbar (next-nprogress-bar)

--- next-app/app/layout.js
+++ next-app/app/layout.js
@@ -3,17 +3,20 @@ import BootstrapClient from "@/components/libraries/Bootstrap";
 import Header from "@/components/layout/Header";
 import Toastify from "@/components/libraries/Toastify";
 import Footer from "@/components/layout/Footer";
+import NextNprogress from "@/components/libraries/NextNprogress";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="fa" dir="rtl">
       <body>
-      <Header />
-      {children}
+      <NextNprogress>
+          <Header />
+          {children}
 
-      <Footer />
-      <BootstrapClient />
-      <Toastify />
+          <Footer />
+          <BootstrapClient />
+          <Toastify />
+      </NextNprogress>
       </body>
     </html>
   );
