1f76a1654ef6ee97c56aeeb61f74f14fb43df298
Hossein <abdollahzadeh.hossein@gmail.com>
2024-12-25 16:12:30 +0330

    Menu page (categories and products and paginate)

---
+++ next-app/components/menu/ProductsList.js
@@ -0,0 +1,19 @@
+import {getFetch} from "@/utils/fetch";
+import Product from "@/components/products/Product";
+import Paginate from "@/components/menu/Paginate";
+
+export default async function ProductsList({params}) {
+    const data = await getFetch(`/menu?${params}`)
+    return (
+        <>
+            <div className="row gx-3">
+                {data.products.map(product => (
+                    <div key={product.id} className="col-sm-6 col-lg-4">
+                        <Product product={product}/>
+                    </div>
+                ))}
+            </div>
+            <Paginate links={data.meta.links} />
+        </>
+    )
+}
