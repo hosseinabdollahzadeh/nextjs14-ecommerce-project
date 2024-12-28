2e524862e4bbbcfd9b8375d29cae878ba2062b72
Hossein <abdollahzadeh.hossein@gmail.com>
2024-12-28 10:51:56 +0330

    Menu page (category filter)

--- next-app/components/menu/CategoriesList.js
+++ next-app/components/menu/CategoriesList.js
@@ -1,4 +1,18 @@
+"use client"
+import {usePathname, useRouter, useSearchParams} from "next/navigation";
+
 export default function CategoriesList({categories}) {
+    const pathname = usePathname()
+    const router = useRouter()
+    const searchParams = useSearchParams()
+
+    function handleClick(id) {
+        const params = new URLSearchParams(searchParams)
+        params.set('category', id)
+        params.delete('page')
+        router.replace(`${pathname}?${params.toString()}`)
+    }
+
     return (
         <div className="filter-list">
             <div className="form-label">
@@ -6,7 +20,11 @@ export default function CategoriesList({categories}) {
             </div>
             <ul>
                 {categories.map(category => (
-                    <li key={category.id} className="my-2 cursor-pointer">{category.name}</li>
+                    <li key={category.id}
+                        onClick={() => handleClick(category.id)}
+                        className={searchParams.has('category') && searchParams.get('category') == category.id ? 'my-2 filter-list-active' : 'my-2 cursor-pointer'}>
+                        {category.name}
+                    </li>
                 ))}
             </ul>
         </div>
