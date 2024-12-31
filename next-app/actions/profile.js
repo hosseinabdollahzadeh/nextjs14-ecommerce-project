5e17cc6a0293ccbb85d948469e0ac3e96530d835
Hossein <abdollahzadeh.hossein@gmail.com>
2024-12-31 12:41:32 +0330

    Create address in profile

--- next-app/actions/profile.js
+++ next-app/actions/profile.js
@@ -23,7 +23,7 @@ async function editInfo(state, formData) {
     }
 
     const token = cookies().get('token');
-    const data = await postFetch('/profile/info/edit', {name, email},{'Authorization': `Bearer ${token.value}`})
+    const data = await postFetch('/profile/info/edit', {name, email}, {'Authorization': `Bearer ${token.value}`})
     if (data.status === 'success') {
         return {
             status: data.status,
@@ -39,4 +39,63 @@ async function editInfo(state, formData) {
 
 }
 
-export {editInfo}
+async function createAddress(state, formData) {
+    const title = formData.get('title')
+    const cellphone = formData.get('cellphone')
+    const postal_code = formData.get('postal_code')
+    const province_id = formData.get('province_id')
+    const city_id = formData.get('city_id')
+    const address = formData.get('address')
+    if (title === '') {
+        return {
+            status: "error",
+            message: "فیلد عنوان الزامی است."
+        }
+    }
+    const cellphonePattern = /^(\+98|0)?9\d{9}$/i;
+    if (cellphone === '' || !cellphonePattern.test(cellphone)) {
+        return {
+            status: "error",
+            message: "فیلد شماره تماس نامعتبر است."
+        }
+    }
+    const postalCodePattern = /^\d{5}[ -]?\d{5}$/i;
+    if (postal_code === '' || !postalCodePattern.test(postal_code)) {
+        return {
+            status: "error",
+            message: "فیلد کد پستی نامعتبر است."
+        }
+    }
+
+    if (address === '') {
+        return {
+            status: "error",
+            message: "فیلد آدرس الزامی است."
+        }
+    }
+
+    const token = cookies().get('token');
+    const data = await postFetch('/profile/addresses/create', {
+        title,
+        cellphone,
+        postal_code,
+        province_id,
+        city_id,
+        address
+    }, {'Authorization': `Bearer ${token.value}`})
+    if (data.status === 'success') {
+        return {
+            status: data.status,
+            message: "ثبت آدرس با موفقیت انجام شد."
+        }
+    } else {
+        return {
+            status: data.status,
+            message: handleError(data.message)
+        }
+    }
+
+
+}
+
+export {editInfo, createAddress}
