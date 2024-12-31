8d5561a38e9142e6a0f7d929cfbc844460f793bb
Hossein <abdollahzadeh.hossein@gmail.com>
2024-12-31 13:28:11 +0330

    Edit address in profile

--- next-app/actions/profile.js
+++ next-app/actions/profile.js
@@ -98,4 +98,69 @@ async function createAddress(state, formData) {
 
 }
 
-export {editInfo, createAddress}
+async function editAddress(state, formData) {
+    const title = formData.get('title')
+    const cellphone = formData.get('cellphone')
+    const postal_code = formData.get('postal_code')
+    const province_id = formData.get('province_id')
+    const city_id = formData.get('city_id')
+    const address = formData.get('address')
+    const address_id = formData.get('address_id')
+    if (address_id === null || address_id === '') {
+        return {
+            status: "error",
+            message: "شناسه آدرس الزامی است."
+        }
+    }
+    if (title === '') {
+        return {
+            status: "error",
+            message: "فیلد عنوان الزامی است."
+        }
+    }
+    const cellphonePattern = /^(\+98|0)?9\d{9}$/i;
+    if (cellphone === '' || !cellphonePattern.test(cellphone)) {
+        return {
+            status: "error",
+            message: "فیلد شماره تماس نامعتبر است."
+        }
+    }
+    const postalCodePattern = /^\d{5}[ -]?\d{5}$/i;
+    if (postal_code === '' || !postalCodePattern.test(postal_code)) {
+        return {
+            status: "error",
+            message: "فیلد کد پستی نامعتبر است."
+        }
+    }
+
+    if (address === '') {
+        return {
+            status: "error",
+            message: "فیلد آدرس الزامی است."
+        }
+    }
+
+    const token = cookies().get('token');
+    const data = await postFetch('/profile/addresses/edit', {
+        address_id,
+        title,
+        cellphone,
+        postal_code,
+        province_id,
+        city_id,
+        address
+    }, {'Authorization': `Bearer ${token.value}`})
+    if (data.status === 'success') {
+        return {
+            status: data.status,
+            message: "ویرایش آدرس با موفقیت انجام شد."
+        }
+    } else {
+        return {
+            status: data.status,
+            message: handleError(data.message)
+        }
+    }
+}
+
+export {editInfo, createAddress, editAddress}
