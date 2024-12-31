5a000801a7ff936169412601eb11fba2fe952ee2
Hossein <abdollahzadeh.hossein@gmail.com>
2024-12-31 10:54:34 +0330

    Edit profile info

--- next-app/app/profile/page.js
+++ next-app/app/profile/page.js
@@ -1,27 +1,14 @@
 import {getFetch} from "@/utils/fetch";
 import {use} from "react";
 import {cookies} from "next/headers";
+import EditForm from "@/components/profile/info/EditForm";
 
 export default async function ProfilePage() {
     const token = cookies().get('token');
     const user = await getFetch("/profile/info", {'Authorization': `Bearer ${token.value}`})
     return (
         <div className="vh-70">
-            <div className="row g-4">
-                <div className="col col-md-6">
-                    <label className="form-label">نام و نام خانوادگی</label>
-                    <input type="text" className="form-control" defaultValue={user.name}/>
-                </div>
-                <div className="col col-md-6">
-                    <label className="form-label">ایمیل</label>
-                    <input type="text" className="form-control" defaultValue={user.email}/>
-                </div>
-                <div className="col col-md-6">
-                    <label className="form-label">شماره تلفن</label>
-                    <input type="text" disabled className="form-control" defaultValue={user.cellphone}/>
-                </div>
-            </div>
-            <button type="submit" className="btn btn-primary mt-4">ویرایش</button>
+            <EditForm user={user}/>
         </div>
     )
 }
