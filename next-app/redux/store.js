8be08e8bcd714269ef8d323408d279a26d814261
Hossein <abdollahzadeh.hossein@gmail.com>
2025-01-12 15:15:02 +0330

    ShoppingCart - store on localstorage using redux-persist package

--- next-app/redux/store.js
+++ next-app/redux/store.js
@@ -2,9 +2,22 @@
 
 import {configureStore} from "@reduxjs/toolkit";
 import {cartReducer} from "@/redux/slices/cartSlice";
+import storage from "redux-persist/lib/storage";
+import {persistStore, persistReducer} from "redux-persist";
 
+const persistConfig = {
+    key: 'root',
+    storage
+}
+
+const persistedReducer = persistReducer(persistConfig, cartReducer)
 export const store = configureStore({
-    reducer:{
-        shoppingCart: cartReducer
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+        serializableCheck: false
+    }),
+    reducer: {
+        shoppingCart: persistedReducer
     }
 })
+
+export const persistor = persistStore(store)
