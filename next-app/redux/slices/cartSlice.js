a0e1e2df933a7300faf387d8750845b2b962d814
Hossein <abdollahzadeh.hossein@gmail.com>
2025-01-18 06:48:22 +0330

    Show items in cart page

--- next-app/redux/slices/cartSlice.js
+++ next-app/redux/slices/cartSlice.js
@@ -17,9 +17,18 @@ export const cartSlice = createSlice({
         removeFromCart: (state, action) => {
             const productId = action.payload
             state.cart = state.cart.filter(p => p.id !== productId)
+        },
+        increment: (state, action) => {
+            state.cart = state.cart.map(p => p.id === action.payload ? {...p, qty: p.qty + 1} : p)
+        },
+        decrement: (state, action) => {
+            state.cart = state.cart.map(p => p.id === action.payload ? {...p, qty: p.qty - 1} : p)
+        },
+        clearCart: (state) => {
+            state.cart = []
         }
     }
 })
 
-export const {addToCart, removeFromCart} = cartSlice.actions
+export const {addToCart, removeFromCart, increment, decrement, clearCart} = cartSlice.actions
 export const cartReducer = cartSlice.reducer
